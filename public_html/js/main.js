8e8d62f6788d7b76ff8dfde77e57164104a16f7c
anka987 <cubicabubica@gmail.com>
2018-06-26 23:49:12 +0200

    first commit

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
 
-// SLIDE UP AND DOWN DROPDOWN LISTS IN NAVIGATION
+    // SLIDE UP AND DOWN DROPDOWN LISTS IN NAVIGATION
     $('.has-submenu .nav-link').click(function (e) {
         e.preventDefault();
+
         if ($(this).next('.shop-products-dropdown').hasClass('opened')) {
             $(this).next('.shop-products-dropdown').removeClass('opened');
         } else {
@@ -10,7 +11,6 @@ $(document).ready(function () {
             $(this).next('.shop-products-dropdown').addClass('opened');
         }
     });
-
 
     // HEADER SEARCH SHOW
     $(".show-search").click(function (e) {
@@ -20,10 +20,24 @@ $(document).ready(function () {
     });
 
 
-    // OWL-CAROUSEL
-    
-    // HEADER SLIDER
+    // TOGGLE CLASS UNDERLINE PINK
+    $('.shop-products-dropdown li ul li').hover(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('font-bold');
+        $(this).parent().prev().children().toggleClass('underline-pink');
+    });
+
+    $('#accordion .card:first-child() h5').css('border-bottom', 'none');
+    $('#accordion .btn').click(function (e) {
+        e.preventDefault();
+        $(this).parent().toggleClass('accordion-border');
+    });
+
+
+
+    //OWL-CAROUSEL
     if ($('.owl-carousel').length > 0) {
+        // HEADER SLIDER
         $('.authors-slider').owlCarousel({
             loop: true,
             margin: 10,
@@ -32,12 +46,9 @@ $(document).ready(function () {
             dots: false,
             navText: ['<', '>']
         });
-    }
-    
-    
-    // FRECKLES SLIDER
-    if ($('.owl-carousel').length > 0) {
-        $('.freckles-slider').owlCarousel({
+
+        // CLASSIC SLIDER
+        $('.classic-slider').owlCarousel({
             loop: true,
             margin: 10,
             items: 1,
@@ -45,39 +56,78 @@ $(document).ready(function () {
             dots: false,
             navText: ['<', '>']
         });
-    }
-    
-    
-    // GEL SLIDER
-    if ($('.owl-carousel').length > 0) {
-        $('.gel-slider').owlCarousel({
+
+        // BRANDS SLIDER
+        $('.brands-slider').owlCarousel({
             loop: true,
             margin: 10,
-            items: 1,
+            items: 6,
             nav: true,
             dots: false,
-            navText: ['<', '>']
+            navText: ['<', '>'],
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                480: {
+                    items: 4
+                },
+                768: {
+                    items: 5
+                },
+                992: {
+                    items: 6
+                }
+            }
         });
+
+        // FRECKLES SLIDER
+        if ($('.owl-carousel').length > 0) {
+            $('.freckles-slider').owlCarousel({
+                loop: true,
+                margin: 10,
+                items: 1,
+                nav: true,
+                dots: false,
+                navText: ['<', '>']
+            });
+        }
+
+        // GEL SLIDER
+        if ($('.owl-carousel').length > 0) {
+            $('.gel-slider').owlCarousel({
+                loop: true,
+                margin: 10,
+                items: 1,
+                nav: true,
+                dots: false,
+                navText: ['<', '>']
+            });
+        }
+
+        // GIRAFE SLIDER
+        if ($('.owl-carousel').length > 0) {
+            $('.girafe-slider').owlCarousel({
+                loop: true,
+                margin: 10,
+                items: 1,
+                nav: true,
+                dots: false,
+                navText: ['<', '>']
+            });
+        }
+
     }
-    
-    
-    
-    // GIRAFE SLIDER
-    if ($('.owl-carousel').length > 0) {
-        $('.girafe-slider').owlCarousel({
-            loop: true,
-            margin: 10,
-            items: 1,
-            nav: true,
-            dots: false,
-            navText: ['<', '>']
-        });
-    }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
 
 });
+
+
+
